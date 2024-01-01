797cd911da5ced6a619a2e07557a277205469342
Sergey87 <serge.techno@gmail.com>
2024-01-02 00:34:50 +0300

    added introduce to react router

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,30 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import App from './App.tsx';
+import { Menu } from './pages/Menu/Menu';
+import { Cart } from './pages/Cart/Cart';
+import { Error } from './pages/Error/Error';
+import './index.css';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Menu />,
+  },
+  {
+    path: '/cart',
+    element: <Cart />,
+  },
+  {
+    path: '*',
+    element: <Error />,
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
+    <RouterProvider router={router} />
     <App />
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
