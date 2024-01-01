6369b15c6a913ffe4f05cbfcb094a515b1cf4734
Sergey87 <serge.techno@gmail.com>
2024-01-01 21:14:29 +0300

    added base components input and button

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -2,10 +2,18 @@ import styles from './Button.module.css';
 import { ButtonProps } from './Button.props';
 import cn from 'classnames';
 
-function Button({ children, className, ...props }: ButtonProps) {
+function Button({
+  children,
+  className,
+  appearence = 'small',
+  ...props
+}: ButtonProps) {
   return (
     <button
-      className={cn(styles['button'], styles['accent'], className)}
+      className={cn(styles['button'], styles['accent'], className, {
+        [styles['small']]: appearence === 'small',
+        [styles['big']]: appearence === 'big',
+      })}
       {...props}
     >
       {children}
