a4b197255ddfb59e5edf01697cf6c59761e80d03
Sergey87 <serge.techno@gmail.com>
2024-01-05 02:40:46 +0300

    added card of product

--- src/components/ProductCard/ProductCard.tsx
+++ src/components/ProductCard/ProductCard.tsx
@@ -1,27 +1,32 @@
+import { Link } from 'react-router-dom';
 import styles from './ProductCard.module.css';
 import { ProductCardProps } from './ProductCard.props';
 
 function ProductCard(props: ProductCardProps) {
   return (
-    <div className={styles['card']}>
-      <div className={styles['head']}>
-        <div className={styles['price']}>
-          {props.price}
-          <span className={styles['currency']}>RUB</span>
+    <Link to={`/product/${props.id}`} className={styles['link']}>
+      <div className={styles['card']}>
+        <div
+          className={styles['head']}
+          style={{ backgroundImage: `url('${props.image}')` }}
+        >
+          <div className={styles['price']}>
+            {props.price}&nbsp;<span className={styles['currency']}>RUB</span>
+          </div>
+          <button className={styles['add-to-cart']}>
+            <img src='/cart-button-icon.svg' alt='cart-button-icon' />
+          </button>
+          <div className={styles['raiting']}>
+            {props.raiting}&nbsp;
+            <img src='/star-icon.svg' alt='star-icon' />
+          </div>
         </div>
-        <button className={styles['add-to-cart']}>
-          <img src='/cart-button-icon.svg' alt='cart-button-icon' />
-        </button>
-        <div className={styles['raiting']}>
-          {props.raiting}
-          <img src='/star-icon.svg' alt='star-icon' />
+        <div className={styles['footer']}>
+          <div className={styles['title']}>{props.title}</div>
+          <div className={styles['description']}>{props.description}</div>
         </div>
       </div>
-      <div className={styles['footer']}>
-        <div className={styles['title']}>{props.title}</div>
-        <div className={styles['description']}>{props.description}</div>
-      </div>
-    </div>
+    </Link>
   );
 }
 
