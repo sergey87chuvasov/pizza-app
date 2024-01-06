a1df38ceb675b0cc69c38bdb81871a743f211682
Sergey87 <serge.techno@gmail.com>
2024-01-06 16:48:24 +0300

    working with requests in a project

---
+++ src/interfaces/product.interface.ts
@@ -0,0 +1,8 @@
+export interface Product {
+  id: number;
+  name: string;
+  ingredients: string[];
+  image: string;
+  rating: number;
+  price: number;
+}
--- src/pages/Menu/Menu.tsx
+++ src/pages/Menu/Menu.tsx
@@ -1,9 +1,52 @@
+import { useEffect, useState } from 'react';
 import Heading from '../../components/Heading/Heading';
 import ProductCard from '../../components/ProductCard/ProductCard';
 import Search from '../../components/Search/Search';
+import { PREFIX } from '../../helpers/API';
+import { Product } from '../../interfaces/product.interface';
 import styles from './Menu.module.css';
+import axios from 'axios';
 
 export function Menu() {
+  const [products, setProducts] = useState<Product[]>([]);
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+
+  const getMenu = async () => {
+    try {
+      // simulated loading processing
+      setIsLoading(true);
+      await new Promise<void>((resolve) => {
+        setTimeout(() => {
+          resolve();
+        }, 2000);
+      });
+      ///
+
+      const { data } = await axios.get<Product[]>(`${PREFIX}/products`);
+      setProducts(data);
+      setIsLoading(false);
+    } catch (e) {
+      console.error(e);
+      setIsLoading(false);
+      return;
+    }
+    // try {
+    //   const res = await fetch(`${PREFIX}/products`);
+    //   if (!res.ok) {
+    //     return;
+    //   }
+    //   const data = (await res.json()) as Product[];
+    //   setProducts(data);
+    // } catch (e) {
+    //   console.error(e);
+    //   return
+    // }
+  };
+
+  useEffect(() => {
+    getMenu();
+  }, []);
+
   return (
     <>
       <div className={styles['head']}>
@@ -11,14 +54,19 @@ export function Menu() {
         <Search placeholder='Введите блюдо или состав' />
       </div>
       <div>
-        <ProductCard
-          id={1}
-          title='Наслаждение'
-          description='Салями, руккола, помидоры, перец'
-          raiting={4.6}
-          price={300}
-          image='/product-demo.png'
-        />
+        {!isLoading &&
+          products.map((p) => (
+            <ProductCard
+              key={p.id}
+              id={p.id}
+              name={p.name}
+              description={p.ingredients.join(', ')}
+              raiting={p.rating}
+              price={p.price}
+              image={p.image}
+            />
+          ))}
+        {isLoading && <>Загружаем продукты...ждите...</>}
       </div>
     </>
   );
