85857d759f42352dea60885cb8a1939bc7c5ceac
Sergey87 <serge.techno@gmail.com>
2024-01-09 16:14:12 +0300

    added private toutes

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -1,4 +1,4 @@
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import Button from '../../components/Button/Button';
 import Heading from '../../components/Heading/Heading';
 import Input from '../../components/Input/Input';
@@ -6,6 +6,7 @@ import styles from './Login.module.css';
 import { FormEvent, useState } from 'react';
 import axios, { AxiosError } from 'axios';
 import { PREFIX } from '../../helpers/API';
+import { LoginResponse } from '../../interfaces/auth.interface';
 
 export type LoginForm = {
   email: {
@@ -17,7 +18,8 @@ export type LoginForm = {
 };
 
 export function Login() {
-  const [error, setError] = useState<string | undefined>();
+  const [error, setError] = useState<string | null>();
+  const navigate = useNavigate();
   const submit = async (e: FormEvent) => {
     e.preventDefault();
     setError(null);
@@ -28,11 +30,13 @@ export function Login() {
 
   const sendLogin = async (email: string, password: string) => {
     try {
-      const { data } = await axios.post(`${PREFIX}/auth/login`, {
+      const { data } = await axios.post<LoginResponse>(`${PREFIX}/auth/login`, {
         email,
         password,
       });
       console.log(data);
+      localStorage.setItem('jwt', data.access_token);
+      navigate('/');
     } catch (e) {
       if (e instanceof AxiosError) {
         console.log(e);
