f14b6fa7a6fdf75b159ad20933ccd58973eab5f9
Sergey87 <serge.techno@gmail.com>
2024-01-10 17:53:05 +0300

    added storage data

--- src/main.tsx
+++ src/main.tsx
@@ -1,17 +1,17 @@
 import React, { Suspense, lazy } from 'react';
 import ReactDOM from 'react-dom/client';
-import { RouterProvider, createBrowserRouter, defer } from 'react-router-dom';
-// import { Menu } from './pages/Menu/Menu';
-import { Cart } from './pages/Cart/Cart';
-import { Error as ErrorPage } from './pages/Error/Error';
 import './index.css';
+import { RouterProvider, createBrowserRouter, defer } from 'react-router-dom';
+import { Cart } from './pages/Cart/Cart.tsx';
+import { Error as ErropPage } from './pages/Error/Error.tsx';
 import { Layout } from './layout/Menu/Layout.tsx';
 import { Product } from './pages/Product/Product.tsx';
 import axios from 'axios';
 import { PREFIX } from './helpers/API.ts';
 import { AuthLayout } from './layout/Auth/AuthLayout.tsx';
-import { Register } from './pages/Register/Register.tsx';
 import { Login } from './pages/Login/Login.tsx';
+import { Register } from './pages/Register/Register.tsx';
+import { RequireAuth } from './helpers/RequireAuth.tsx';
 import { Provider } from 'react-redux';
 import { store } from './store/store.ts';
 
@@ -20,7 +20,11 @@ const Menu = lazy(() => import('./pages/Menu/Menu'));
 const router = createBrowserRouter([
   {
     path: '/',
-    element: <Layout />,
+    element: (
+      <RequireAuth>
+        <Layout />
+      </RequireAuth>
+    ),
     children: [
       {
         path: '/',
@@ -37,7 +41,7 @@ const router = createBrowserRouter([
       {
         path: '/product/:id',
         element: <Product />,
-        errorElement: <>!Ошибка!</>,
+        errorElement: <>Ошибка</>,
         loader: async ({ params }) => {
           return defer({
             data: new Promise((resolve, reject) => {
@@ -49,20 +53,6 @@ const router = createBrowserRouter([
               }, 2000);
             }),
           });
-
-          //   data: await axios
-          //     .get(`${PREFIX}/products/${params.id}`)
-          //     .then((data) => data),
-          // });
-
-          // await new Promise<void>((resolve) => {
-          //   throw new Error('errrorrr');
-          //   setTimeout(() => {
-          //     resolve();
-          //   }, 2000);
-          // });
-          // const { data } = await axios.get(`${PREFIX}/products/${params.id}`);
-          // return data;
         },
       },
     ],
@@ -83,7 +73,7 @@ const router = createBrowserRouter([
   },
   {
     path: '*',
-    element: <ErrorPage />,
+    element: <ErropPage />,
   },
 ]);
 
--- src/store/user.slice.ts
+++ src/store/user.slice.ts
@@ -1,19 +1,25 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { PayloadAction, createSlice } from '@reduxjs/toolkit';
+import { loadState } from './storage';
 
+export const JWT_PERSISTENT_STATE = 'userData';
 export interface UserState {
   jwt: string | null;
 }
 
+export interface UserPersistentState {
+  jwt: string | null;
+}
+
 const initialState: UserState = {
-  jwt: null,
+  jwt: loadState<UserPersistentState>(JWT_PERSISTENT_STATE)?.jwt ?? null,
 };
 
 export const userSlice = createSlice({
   name: 'user',
   initialState,
   reducers: {
-    addJwt: (state) => {
-      state.jwt = 'test';
+    addJwt: (state, action: PayloadAction<string>) => {
+      state.jwt = action.payload;
     },
     logout: (state) => {
       state.jwt = null;
