80d34dcf004b26b213a20b6aff81c326bc5e9926
Sergey87 <serge.techno@gmail.com>
2024-01-12 10:27:43 +0300

    added registration

--- src/pages/Register/Register.tsx
+++ src/pages/Register/Register.tsx
@@ -1,3 +1,79 @@
+import { Link, useNavigate } from 'react-router-dom';
+import Button from '../../components/Button/Button';
+import Heading from '../../components/Heading/Heading';
+import Input from '../../components/Input/Input';
+import styles from './Login.module.css';
+import { FormEvent, useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { AppDispath } from '../../store/store';
+import { RootState } from '../../store/store';
+import { login, userActions } from '../../store/user.slice';
+
+export type RegisterForm = {
+  email: {
+    value: string;
+  };
+  password: {
+    value: string;
+  };
+  name: {
+    value: string;
+  };
+};
+
 export function Register() {
-  return <>Register</>;
+  const navigate = useNavigate();
+  const dispatch = useDispatch<AppDispath>();
+  const { jwt, loginErrorMessage } = useSelector((s: RootState) => s.user);
+
+  useEffect(() => {
+    if (jwt) {
+      navigate('/');
+    }
+  }, [jwt, navigate]);
+
+  const submit = async (e: FormEvent) => {
+    e.preventDefault();
+    dispatch(userActions.clearLoginError());
+    const target = e.target as typeof e.target & LoginForm;
+    const { email, password } = target;
+    await sendLogin(email.value, password.value);
+  };
+
+  const sendLogin = async (email: string, password: string) => {
+    dispatch(login({ email, password }));
+  };
+
+  return (
+    <div className={styles['login']}>
+      <Heading>Вход</Heading>
+      {loginErrorMessage && (
+        <div className={styles['error']}>{loginErrorMessage}</div>
+      )}
+      <form className={styles['form']} onSubmit={submit}>
+        <div className={styles['field']}>
+          <label htmlFor='email'>Ваш email</label>
+          <Input id='email' placeholder='Email' name='email' />
+        </div>
+        <div className={styles['field']}>
+          <label htmlFor='password'>Ваш пароль</label>
+          <Input
+            id='password'
+            type='password'
+            placeholder='Password'
+            name='password'
+          />
+        </div>
+        <div className={styles['field']}>
+          <label htmlFor='password'>Ваше имя</label>
+          <Input id='name' placeholder='Имя' name='name' />
+        </div>
+        <Button appearence='big'>Зарегистрироваться</Button>
+      </form>
+      <div className={styles['links']}>
+        <div>Есть аккаунт?</div>
+        <Link to='/auth/login'>Войти</Link>
+      </div>
+    </div>
+  );
 }
